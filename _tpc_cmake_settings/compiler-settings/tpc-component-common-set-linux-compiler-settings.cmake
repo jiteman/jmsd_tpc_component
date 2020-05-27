57c9346614263e749f4d03f26b2b1c7b308e17c5
jiteman <jiteman@gmail.com>
2020-05-27 13:38:33 +0300

    C configuration templates have been added

--- _tpc_cmake_settings/compiler-settings/tpc-component-common-set-linux-compiler-settings.cmake
+++ _tpc_cmake_settings/compiler-settings/tpc-component-common-set-linux-compiler-settings.cmake
@@ -2,7 +2,7 @@ JMSD_CMAKE_CURRENT_FILE_IN( "${JMSD_FOREIGN_COMPONENT_BASE_NAME}-common-set-linu
 
 if ( UNIX )
 	# C
-	## set( ${PROJECT_NAME}_C_FLAGS ${CMAKE_C_FLAGS} )
+	##set( ${PROJECT_NAME}_C_FLAGS ${CMAKE_C_FLAGS} )
 
 	## list( APPEND ${PROJECT_NAME}_C_FLAGS "-Wno-" ) #
 
@@ -10,10 +10,10 @@ if ( UNIX )
 
 	## string( REPLACE "-W" "" ${PROJECT_NAME}_C_FLAGS_STR "${${PROJECT_NAME}_C_FLAGS_STR}" ) #
 
-	## set( CMAKE_C_FLAGS ${${PROJECT_NAME}_C_FLAGS_STR} )
+	##set( CMAKE_C_FLAGS ${${PROJECT_NAME}_C_FLAGS_STR} )
 
 	# C++
-	## set( ${PROJECT_NAME}_CXX_FLAGS ${CMAKE_CXX_FLAGS} )
+	##set( ${PROJECT_NAME}_CXX_FLAGS ${CMAKE_CXX_FLAGS} )
 
 	## list( APPEND ${PROJECT_NAME}_CXX_FLAGS "-Wno-" ) #
 
@@ -21,7 +21,7 @@ if ( UNIX )
 
 	## string( REPLACE "-W" "" ${PROJECT_NAME}_CXX_FLAGS_STR "${${PROJECT_NAME}_CXX_FLAGS_STR}" ) #
 
-	## set( CMAKE_CXX_FLAGS ${${PROJECT_NAME}_CXX_FLAGS_STR} )
+	##set( CMAKE_CXX_FLAGS ${${PROJECT_NAME}_CXX_FLAGS_STR} )
 else()
 	message( SEND_ERROR "[JMSD] ${JMSD_FOREIGN_COMPONENT_BASE_NAME}-common COMPILER SETTINGS: ${JMSD_FOREIGN_COMPONENT_BASE_NAME}-common-set-linux-compiler-settings.cmake is included while not on linux" )
 
